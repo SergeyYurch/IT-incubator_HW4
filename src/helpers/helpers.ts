ef25d1eb560c16e7cfc61f8ae5272996d1950139
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-11-30 21:30:55 +0200

    add helpers.ts

---
+++ src/helpers/helpers.ts
@@ -0,0 +1,11 @@
+import {Request} from "express";
+import {PaginatorOptionInterface} from "../repositories/queryRepository.interface";
+
+export const parseQueryPaginator = (req: Request): PaginatorOptionInterface => {
+    return {
+        pageNumber: req.query.pageNumber ? +req.query.pageNumber : 1,
+        pageSize: req.query.pageSize ? +req.query.pageSize : 10,
+        sortBy: req.query.sortBy ? String(req.query.sortBy) : 'createdAt',
+        sortDirection: req.query.sortDirection === 'desc' ? 'desc' : 'asc'
+    };
+};
