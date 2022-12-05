ab935b3a83169c05858946db9f1a7467bc7e69f0
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-12-05 23:03:43 +0200

    refactor app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import {testingRouter} from "./controllers/testing.controller";
 import {blogsRouter} from "./controllers/blogs.controller";
 import {postsRouter} from "./controllers/posts.controller";
 import {authGuardMiddleware} from "./middlewares/authGuard.middleware";
+import {authRouter} from "./controllers/auth.controller";
+import {usersRouter} from "./controllers/users.controller";
 
 export const app = express();
 app.use(express.json())
@@ -10,4 +12,6 @@ app.use(authGuardMiddleware);
 app.use('/blogs', blogsRouter)
 app.use('/posts', postsRouter)
 app.use('/testing', testingRouter)
+app.use('/auth', authRouter)
+app.use('/users', usersRouter)
 
