5e5cf0dd3ac3fa87f84f22fe5b9b0b4b9712cc1f
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-12-06 21:01:46 +0200

    added clearing usersCollection to repository.ts

--- src/repositories/repository.ts
+++ src/repositories/repository.ts
@@ -1,14 +1,15 @@
 import {
     RepositoryInterface
 } from "./repository.interface";
-import {blogsCollection, postsCollection} from "./db";
+import {blogsCollection, postsCollection, usersCollection} from "./db";
 
 export const repository: RepositoryInterface = {
     dataBaseClear: async (): Promise<boolean> => {
         console.log(`[repository]:start dataBaseClear`);
         const resultBlogs = await blogsCollection.deleteMany({});
         const resultPosts = await postsCollection.deleteMany({});
-        return resultBlogs.acknowledged && resultPosts.acknowledged;
+        const resultUsers = await usersCollection.deleteMany({});
+        return resultBlogs.acknowledged && resultPosts.acknowledged && resultUsers.acknowledged;
     }
 
 };
