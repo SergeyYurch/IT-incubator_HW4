f34b82a23b9c5142d3554c6ae858096fef9c4998
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-12-08 21:34:39 +0200

    refactor app.ts delete authGuardMddleware

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,11 @@ import express from 'express';
 import {testingRouter} from "./controllers/testing.controller";
 import {blogsRouter} from "./controllers/blogs.controller";
 import {postsRouter} from "./controllers/posts.controller";
-import {authGuardMiddleware} from "./middlewares/authGuard.middleware";
 import {authRouter} from "./controllers/auth.controller";
 import {usersRouter} from "./controllers/users.controller";
 
 export const app = express();
 app.use(express.json())
-app.use(authGuardMiddleware);
 app.use('/blogs', blogsRouter)
 app.use('/posts', postsRouter)
 app.use('/testing', testingRouter)
