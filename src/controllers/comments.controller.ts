c933531cd2915b40d10fd71e9a84fffef2dfa8d9
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-12-10 22:20:19 +0200

    added comments.controller.ts

--- src/controllers/comments.controller.ts
+++ src/controllers/comments.controller.ts
@@ -1,16 +1,13 @@
 import {Router, Request, Response} from "express";
 import {validatorMiddleware} from "../middlewares/validator.middleware";
 import {
-    RequestWithBody,
-    RequestWithId
+    RequestWithId, RequestWithIdAndBody
 } from "../types/request.type";
 import {queryRepository} from "../repositories/query.repository";
-import {PaginatorOptionInterface} from "../repositories/interfaces/query.repository.interface";
-import {parseQueryPaginator} from "../helpers/helpers";
 import {ObjectId} from "mongodb";
-import {usersService} from "../services/users.service";
-import {UserInputModelDto} from "./dto/userInputModel.dto";
 import {authBearerMiddleware} from "../middlewares/authBearer.middleware";
+import {commentsService} from "../services/comments.service";
+import {CommentInputModelDto} from "./dto/commentInputModel.dto";
 
 export const commentsRouter = Router();
 
@@ -18,39 +15,45 @@ const {
     validateCommentInputModel,
     validateResult
 } = validatorMiddleware;
-const {createUser, deleteUserById, findUserByEmailOrPassword, getUserById} = usersService;
-const {getAllUsers} = queryRepository;
+const {deleteCommentById, editComment} = commentsService;
+const {getCommentById} = queryRepository;
 
 
 commentsRouter.put('/:commentId',
     authBearerMiddleware,
     validateCommentInputModel(),
     validateResult,
-    async (req: RequestWithBody<UserInputModelDto>, res: Response) => {
-        const {login, password, email} = req.body;
-        if (await findUserByEmailOrPassword(login)
-            || await findUserByEmailOrPassword(email)) return res.sendStatus(400);
-        const result = await createUser(login, email, password);
-        return result ? res.status(201).json(result) : res.sendStatus(500);
+    async (req: RequestWithIdAndBody<CommentInputModelDto>, res: Response) => {
+        const id = req.params.commentId;
+        console.log(`[commentsController]:PUT - edit comment by ID: ${id}`);
+        if (!ObjectId.isValid(id)) return res.sendStatus(404);
+        const userId = req.user!.id;
+        const {content} = req.body;
+        const commentInDB = await getCommentById(id);
+        if (!commentInDB) return res.sendStatus(404);
+        if (commentInDB.userId !== userId) return res.sendStatus(403);
+        const result = await editComment(id, {content});
+        console.log(`[commentsController]:PUT - edit comment by ID: ${id} result: ${result}`);
+        return result ? res.sendStatus(204) : res.sendStatus(500);
     });
 
+commentsRouter.delete('/:commentId',
+    authBearerMiddleware,
+    async (req: RequestWithId, res: Response) => {
+        const id = req.params.commentId;
+        if (!ObjectId.isValid(id) || !await getCommentById(id)) return res.sendStatus(404);
+        const userId = req.user!.id;
+        const commentInDB = await getCommentById(id);
+        if (!commentInDB) return res.sendStatus(404);
+        if (commentInDB.userId !== userId) return res.sendStatus(403);
+        const result = await deleteCommentById(id);
+        return result ? res.sendStatus(204) : res.sendStatus(500);
+    });
 
 commentsRouter.get('/:commentId',
     async (req: Request, res: Response) => {
-    const searchLoginTerm: string | null = req.query.searchLoginTerm ? String(req.query.searchLoginTerm) : null;
-    const searchEmailTerm: string | null = req.query.searchEmailTerm ? String(req.query.searchEmailTerm) : null;
-    const paginatorOption: PaginatorOptionInterface = parseQueryPaginator(req);
-    const result = await getAllUsers(paginatorOption, searchLoginTerm, searchEmailTerm);
-    return res.status(200).json(result);
-});
-
-commentsRouter.delete('/:id',
-    authBearerMiddleware,
-    async (req: RequestWithId, res: Response) => {
-    const id = req.params.id;
-    if (!ObjectId.isValid(id)) return res.sendStatus(404);
-    const isExistUser = await getUserById(id);
-    if (!isExistUser) return res.sendStatus(404)
-    const result = await deleteUserById(id);
-    return result ? res.sendStatus(204) : res.sendStatus(401);
-});
+        const id = req.params.commentId;
+        console.log(`[commentsController]:GET - get comment by ID: ${id}`);
+        const result = await getCommentById(id);
+        return result ? res.status(200).json(result) : res.sendStatus(404);
+    });
