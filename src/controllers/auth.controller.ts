dcc5666f4698b2c7afc6f0ac459259f11516edee
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-12-10 22:18:51 +0200

    fix auth.controller.ts

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,9 +1,10 @@
-import {Router,  Response} from "express";
+import {Router, Response} from "express";
 import {validatorMiddleware} from "../middlewares/validator.middleware";
 import {RequestWithBody} from "../types/request.type";
 import {LoginInputModel} from "./dto/loginInputModel.dto";
 import {usersService} from "../services/users.service";
 import {jwtService} from "../helpers/jwt-service";
+import {authBearerMiddleware} from "../middlewares/authBearer.middleware";
 
 export const authRouter = Router();
 
@@ -17,17 +18,29 @@ authRouter.post('/login',
     validateAuthInputModel(),
     validateResult,
     async (req: RequestWithBody<LoginInputModel>, res: Response) => {
-        const { loginOrEmail, password} = req.body;
+        const {loginOrEmail, password} = req.body;
         console.log(`!!!![authRouter] login:${loginOrEmail}, pass: ${password}`);
-        debugger
         const user = await checkCredentials({loginOrEmail, password});
-        if (user){
-            const token= await jwtService.createJWT(user)
+        if (user) {
+            const token = await jwtService.createJWT(user.id);
             return res.status(200).send({
                 "accessToken": token
-            })
+            });
         } else {
             return res.sendStatus(401);
         }
     });
 
+authRouter.get('/me',
+    authBearerMiddleware,
+    async (req: RequestWithBody<LoginInputModel>, res: Response) => {
+        const userId = req.user!.id;
+        console.log(`[authController]:get user info by ID: ${userId}`);
+        const userInDb = await usersService.getUserById(userId);
+        if (!userInDb) return res.sendStatus(401);
+        return res.status(200). send({
+            email: userInDb.email,
+            login: userInDb.login,
+            userId
+        });
+    });
