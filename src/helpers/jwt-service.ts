6b0f3bd731fed162c2d8f09eb64b46d3dc65339a
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-12-10 22:21:09 +0200

    added jwt-service.ts

--- src/helpers/jwt-service.ts
+++ src/helpers/jwt-service.ts
@@ -1,15 +1,13 @@
 import * as dotenv from "dotenv";
-import { WithId} from "mongodb";
 import jwt from 'jsonwebtoken';
-import {UserEntity} from "../services/entities/user.entity";
 
 dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET || '1';
 
 export const jwtService = {
-    async createJWT(user: WithId<UserEntity>) {
-        return jwt.sign({userId: user._id.toString()}, JWT_SECRET, {expiresIn: '10h'});
+    async createJWT(id:string) {
+        return jwt.sign({userId: id}, JWT_SECRET, {expiresIn: '10h'});
     },
 
     async getUserIdByJwtToken(token: string) {
